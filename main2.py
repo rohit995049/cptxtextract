bc5e0f3e729d0841fbd8af76642a5aedc51f0a9d
rohit995049 <161129526+rohit995049@users.noreply.github.com>
2024-06-05 07:53:10 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -50,9 +50,9 @@ logging.basicConfig(
 
 bot = Client(
     "bot",
-    api_id= 22779671,
-    api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
+    api_id= 27498866,
+    api_hash= "96fbb6ad2e11ab04e83ca09ef3f42455",    
+    bot_token= "7397883354:AAFnga6jdQYs_L__r5nW-Q8CXqW2-H0rPFw"
 )
 
 @bot.on_message(filters.command(["start"]))
